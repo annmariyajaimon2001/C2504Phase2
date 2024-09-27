4d7b5d90cb341f35ad8365400b17cfb6009cae33
annmariyajaimon2001 <annammu2001@gmail.com>
2024-09-27 17:42:14 +0530

    25-09-2024

---
+++ Assignment1Animal/Assignment1Animal/Duck.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Assignment1Animal
+{
+    partial class DuckFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbDuck = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDuck)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbDuck
+            // 
+            this.pbDuck.Image = global::Assignment1Animal.Properties.Resources.duck;
+            this.pbDuck.Location = new System.Drawing.Point(24, 24);
+            this.pbDuck.Name = "pbDuck";
+            this.pbDuck.Size = new System.Drawing.Size(754, 403);
+            this.pbDuck.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbDuck.TabIndex = 0;
+            this.pbDuck.TabStop = false;
+            // 
+            // DuckFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbDuck);
+            this.Name = "DuckFrom";
+            this.Text = "Duck";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmDuck_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pbDuck)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbDuck;
+    }
+}
