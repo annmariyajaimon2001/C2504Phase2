c745cd12eae1f85e0dd1bb9cb621e16f9aa8205e
annmariyajaimon2001 <annammu2001@gmail.com>
2024-10-03 09:11:39 +0530

    02-10-2024

---
+++ CounterProject/CounterProject/CounterViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using System.Security.Cryptography;
+
+namespace CounterProject
+{
+    public class CounterViewModel : INotifyPropertyChanged
+    {
+        //
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+        //        
+        private int _newCounter;
+        public int NewCounter
+        {
+            get => _newCounter;
+            set { _newCounter = value; OnPropertyChanged(nameof(NewCounter)); }
+        }
+        
+
+        public ICommand PlusCommand { get; }
+        public ICommand MinusCommand { get; }
+        
+        //
+        public CounterViewModel()
+        {
+            NewCounter = 0;
+            PlusCommand = new RelayCommand(Increament);
+            MinusCommand = new RelayCommand(Decreament);
+        }
+        
+        public void Increament()
+        {
+
+            NewCounter++;
+            
+        }
+        public void Decreament()
+        {
+            NewCounter--;
+            
+        }
+        
+    }
+}
