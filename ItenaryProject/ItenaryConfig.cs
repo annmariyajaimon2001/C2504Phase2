0aa6071a5a18bef1e303ce9003900be3ea686af7
annmariyajaimon2001 <annammu2001@gmail.com>
2024-10-03 10:02:46 +0530

    30-09-2024

---
+++ ItenaryProject/ItenaryConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ItenaryProject
+{
+    static class ItenaryConfig
+    {
+        public static Window FrmNewIternary { get; set; }
+
+        public static Window FrmEditIternary { get; set; }
+        //view model
+        public static IternaryViewModel VueModel { get; set; }
+
+        static ItenaryConfig()
+        {
+            VueModel = new IternaryViewModel();
+            FrmNewIternary = new NewItenaryWindow();
+            FrmEditIternary = new EditItenaryWindow();
+
+        }
+    }
+}
---
+++ ItenaryProject/ItenaryListWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItenaryProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ItenaryListWindow : Window
+    {
+        public ItenaryListWindow()
+        {
+            InitializeComponent();
+            this.DataContext = ItenaryConfig.VueModel;
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+            ItenaryConfig.FrmNewIternary.Show();
+            NewItenaryWindow newIternaryWindow = (NewItenaryWindow)ItenaryConfig.FrmNewIternary;
+            ItenaryConfig.VueModel.NewWindowClose = newIternaryWindow.WindowClose;
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if (grdIternaries.SelectedIndex == -1)
+            {
+                var result = MessageBox.Show(messageBoxText: "Please select iternary to edit",
+                    caption: "Alert",
+                    button: MessageBoxButton.OK,
+                    icon: MessageBoxImage.Information);
+                return;
+            }
+            ItenaryConfig.FrmEditIternary.Show();
+            EditItenaryWindow editIternaryWindow = (EditItenaryWindow)ItenaryConfig.FrmEditIternary;
+            ItenaryConfig.VueModel.EditWindowClose = editIternaryWindow.WindowClose;
+        }
+
+        private void Window_Closed_1(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
